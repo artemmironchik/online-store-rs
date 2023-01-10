c58e61c89002e480af7a309e40c3e4ff18e10f6c
Artem Mironchik <tawerka.power@gmail.com>
2023-01-10 03:03:17 +0300

    feat: add logo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,6 +7,7 @@ const Layout: FC = () => {
     <div className="min-h-screen">
       <header className="border-b-2 mb-2 z-10 sticky w-full top-0 bg-white">
         <div className="mx-3 sm:mx-5 md:mx-8 lg:mx-14 xl:mx-20 2xl:mx-32 text-xl flex py-2">
+          <h3>Online Store</h3>
           <nav className="ml-auto flex space-x-4">
             <NavLink end to={"/"} className={getNavLinkClass}>
               Main
